2bc48ccc6ecdbde85c71e486167c692ce4c974e0
Vadim Kostrov <vmkvo15@outlook.com>
2023-05-20 21:39:02 +0400

    Refactor weather API code & create info adder function

--- modules/weatherAPI.js
+++ modules/weatherAPI.js
@@ -1,33 +1,49 @@
-async function getData(string) {
+// Universal Get Data Async Function
+export default async function getData(string) {
   const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=9f51d71599cf4a5ba3782327231605&q=${string}`);
   const data = await response.json();
-  console.log(data);
   return data;
 }
 
-async function getWeatherStats(string) {
+// Weather Stat Component
+export async function getWeatherStats(string) {
   const data = await getData(string);
   const stats = data.current;
   return stats;
 }
 
-async function getLocationData(string) {
+export async function getFeelTemp(type, string) {
+  const weatherData = await getWeatherStats(string);
+  const tempNum = (type === 'c') ? weatherData.temp_c : weatherData.temp_f;
+  return `${tempNum} ${type.toUpperCase()}`;
+}
+
+export async function getTemp(type, string) {
+  const weatherData = await getWeatherStats(string);
+  const tempNum = (type === 'c') ? await weatherData.temp_c : await weatherData.temp_f;
+  return `${tempNum} ${type.toUpperCase()}`;
+}
+
+export async function getWindSpeed(string) {
+  const { wind_kph } = await getWeatherStats(string);
+  const windString = `${wind_kph} km/h`;
+  return windString;
+}
+
+export async function getImage(string) {
+  const weatherData = await getWeatherStats(string);
+  const { icon } = weatherData.condition;
+  return icon;
+}
+
+export async function getPressure(string) {
+  const { pressure_mb } = await getWeatherStats(string);
+  return `${pressure_mb} mb`;
+}
+
+// Location stat component
+export async function getLocationData(string) {
   const data = await getData(string);
   const stats = data.location;
   return stats;
-}
-
-getLocationData('dubai');
-
-async function getFeelTemp(type, string) {
-  const weatherData = await getWeatherStats(string);
-  const temp = (type === 'c') ? weatherData.temp_c : weatherData.temp_f;
-  const result = temp + type;
-  return result;
-}
-
-async function getTemp(type, string) {
-  const weatherData = await getWeatherStats(string);
-  const temp = (type === 'c') ? await weatherData.temp_c : await weatherData.temp_f;
-  console.log(`The temperature is ${temp}${type}`);
 }
--- src/index.js
+++ src/index.js
@@ -1,38 +1,24 @@
 import './style.css';
+import getData, {
+  getWeatherStats, getLocationData, getFeelTemp, getTemp, getWindSpeed, getImage, getPressure,
+} from '../modules/weatherAPI';
 
-async function getData(string) {
-  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=9f51d71599cf4a5ba3782327231605&q=${string}`);
-  const data = await response.json();
-  console.log(data);
-  return data;
+async function addInfo(string) {
+  const container = document.querySelector('.container');
+
+  const temp = document.createElement('p');
+  const windSpeed = document.createElement('p');
+  const pressure = document.createElement('p');
+  const image = new Image();
+  image.src = await getImage(string);
+
+  temp.innerHTML = await getTemp('c', string);
+  windSpeed.innerHTML = await getWindSpeed(string);
+  pressure.innerHTML = await getPressure(string);
+  container.append(temp, windSpeed, pressure, image);
 }
 
-async function getWeatherStats(string) {
-  const data = await getData(string);
-  const stats = data.current;
-  return stats;
-}
-
-async function getLocationData(string) {
-  const data = await getData(string);
-  const stats = data.location;
-  return stats;
-}
-
-getLocationData('dubai');
-
-async function getFeelTemp(type, string) {
-  const weatherData = await getWeatherStats(string);
-  const temp = (type === 'c') ? weatherData.temp_c : weatherData.temp_f;
-  const result = temp + type;
-  return result;
-}
-
-async function getTemp(type, string) {
-  const weatherData = await getWeatherStats(string);
-  const temp = (type === 'c') ? await weatherData.temp_c : await weatherData.temp_f;
-  console.log(`The temperature is ${temp}${type}`);
-}
+addInfo('Manila');
 
 // getTemp('c', 'dubai');
 // getFeelTemp('c', 'dubai');
