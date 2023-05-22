46507eafb0b883c0ea1e60fdd8f3240db225f352
Vadim Kostrov <vmkvo15@outlook.com>
2023-05-22 18:14:54 +0400

    Add getData function and debugging print function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+import weatherAPI from './modules/weatherAPI';
+
+async function print(string) {
+  const API = await weatherAPI;
+  API.getData(string);
+}
+
+// print('dubai');
