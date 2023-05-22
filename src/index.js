7eb00a35278b221998088f65df053efc84a41c77
Vadim Kostrov <vmkvo15@outlook.com>
2023-05-22 22:42:29 +0400

    Change module location and add getTemperature function

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import weatherAPI from './modules/weatherAPI';
 async function print(string) {
   const API = await weatherAPI;
   API.getData(string);
+  API.getTemperature(string);
 }
 
-// print('dubai');
+print('dubai');
---
+++ src/modules/weatherAPI.js
@@ -0,0 +1,22 @@
+/* eslint-disable prefer-const */
+const weatherAPI = (async () => {
+  let tempType = 'c';
+
+  async function getData(string) {
+    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=9f51d71599cf4a5ba3782327231605&q=${string}`);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  }
+
+  async function getTemperature(string) {
+    const data = await getData(string);
+    const temp = (tempType === 'c') ? data.current.temp_c : data.current.temp_f;
+    console.log(temp);
+    return temp;
+  }
+
+  return { getData, getTemperature };
+})();
+
+export default weatherAPI;
