a6de03b093abd22c1c29e87976672dbdfe92539e
Vadim Kostrov <vmkvo15@outlook.com>
2023-05-24 23:46:49 +0400

    Remove console log statements

--- src/modules/weatherAPI.js
+++ src/modules/weatherAPI.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /* eslint-disable prefer-const */
 const weatherAPI = (async () => {
   let tempType = 'c';
@@ -11,14 +12,12 @@ const weatherAPI = (async () => {
   async function getTemperature(string) {
     const data = await getData(string);
     const temp = (tempType === 'c') ? data.current.temp_c : data.current.temp_f;
-    console.log(`Real Temp: ${temp} ${tempType.toUpperCase()}`);
     return temp;
   }
 
   async function getFeelTemperature(string) {
     const data = await getData(string);
     const temp = (tempType === 'c') ? data.current.feelslike_c : data.current.feelslike_f;
-    console.log(`Feel Temp: ${temp} ${tempType.toUpperCase()}`);
     return temp;
   }
 
@@ -26,7 +25,6 @@ const weatherAPI = (async () => {
     const data = await getData(string);
     const forecast = data.forecast.forecastday[0];
     const temp = (tempType === 'c') ? forecast.day.mintemp_c : forecast.day.mintemp_f;
-    console.log(`Min Temp: ${temp} ${tempType.toUpperCase()}`);
     return temp;
   }
 
@@ -34,7 +32,6 @@ const weatherAPI = (async () => {
     const data = await getData(string);
     const forecast = data.forecast.forecastday[0];
     const temp = (tempType === 'c') ? forecast.day.maxtemp_c : forecast.day.maxtemp_f;
-    console.log(`Max Temp: ${temp} ${tempType.toUpperCase()}`);
     return temp;
   }
 
@@ -42,7 +39,6 @@ const weatherAPI = (async () => {
     const data = await getData(string);
     const forecast = data.forecast.forecastday[0];
     const temp = (tempType === 'c') ? forecast.day.avgtemp_c : forecast.day.avgtemp_f;
-    console.log(`Avg Temp: ${temp} ${tempType.toUpperCase()}`);
     return temp;
   }
 
@@ -53,35 +49,30 @@ const weatherAPI = (async () => {
   async function getCondition(string) {
     const data = await getData(string);
     const condition = data.current.condition.text;
-    console.log(`Condition: ${condition}`);
     return condition;
   }
 
   async function getWindSpeed(string) {
     const data = await getData(string);
     const { wind_kph } = data.current;
-    console.log(`Wind Speed: ${wind_kph} km/h`);
     return wind_kph;
   }
 
   async function getWindDirection(string) {
     const data = await getData(string);
     const { wind_dir } = data.current;
-    console.log(`Wind Direction: ${wind_dir}`);
     return wind_dir;
   }
 
   async function getPressure(string) {
     const data = await getData(string);
     const { pressure_mb } = data.current;
-    console.log(`Pressure: ${pressure_mb} mb`);
     return pressure_mb;
   }
 
   async function getPrecipitation(string) {
     const data = await getData(string);
     const { precip_mm } = data.current;
-    console.log(`Precipitation(mm): ${precip_mm} mm`);
     return precip_mm;
   }
 
